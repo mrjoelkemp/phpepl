3c3dc5b3710115dbc54be9f399de7e624952556e
Joel Kemp <mrjoelkemp@gmail.com>
2012-11-04 23:57:27 -0500

    On bad words better

--- eval/index.php
+++ eval/index.php
@@ -9,7 +9,13 @@
 	$bad = array("phpInfo", "file_get_contents");
 	
 	$code = str_replace($toRemove, "", $code);
-	$code = str_replace($bad, 'print', $code);
+	
+	foreach ($bad as $badword) {
+		$pos = strpos($code, $badword);
+		if (! pos) {
+			$code = 'echo "Butts";';
+		}
+	}
 	
 	// TODO: Need to prevent the parse error from bade code.
 	eval($code);
