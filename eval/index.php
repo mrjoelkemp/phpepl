0a6e580a3b884e75eff0df92261a0a0c580ecbad
Joel Kemp <mrjoelkemp@gmail.com>
2012-11-04 23:53:31 -0500

    On bad words

--- eval/index.php
+++ eval/index.php
@@ -5,8 +5,11 @@
 	$code = $_POST['code'];
 	
 	// Remove some unsafe and error prone snippets
-	$toRemove = array("<?php", "?>", "<?", "phpInfo()", "file_get_contents");
+	$toRemove = array("<?php", "?>", "<?");
+	$bad = array("phpInfo", "file_get_contents");
+	
 	$code = str_replace($toRemove, "", $code);
+	$code = str_replace($bad, 'print', $code);
 	
 	// TODO: Need to prevent the parse error from bade code.
 	eval($code);
