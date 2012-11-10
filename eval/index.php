29781672c498414a78d6e7cfc9beae39b5468f5b
Joel Kemp <mrjoelkemp@gmail.com>
2012-11-10 16:22:01 -0500

    Added error handling and styling

--- eval/index.php
+++ eval/index.php
@@ -1,16 +1,121 @@
 <?php
-
+	// Turn off errors since eval will throw them on invalid syntax
+	$inString = @ini_set('log_errors', false);
+    $token = @ini_set('display_errors', true);
+    
+	// TODO: Move to JSONP
 	header("Access-Control-Allow-Origin: *");
 	
 	$code = $_POST['code'];
 	
 	// Remove some unsafe and error prone snippets
-	$toRemove = array("<?php", "?>", "<?");
-	$bad = array("phpInfo", "file_get_contents");
+	$toRemove 	= array("<?php", "?>", "<?");
+	$badMethods	= array("phpInfo", "file_get_contents");
+	$code = str_replace(array_merge($toRemove, $badMethods), "", $code);
 	
-	$code = str_replace($toRemove, "", $code);
-	$code = str_replace($bad, "", $code);
+	ob_start();
 	
-	// TODO: Need to prevent the parse error from bade code.
 	eval($code);
+	$result = ob_get_clean();
+	$error = error_get_last();
+	$json = json_encode(array("result" => $result, "error" => $error));
+	echo $json;
+	
+	@ini_set('display_errors', $token);
+	@ini_set('log_errors', $inString);
+	
+	// Parse the code using token_get_all and process the tokens
+	// to determine if there's a syntax error. 
+	// Eval the code and output buffer to prevent leaking the error msg
+	function php_syntax_error($code) {
+	    $braces = 0;
+	    $inString = 0;
+
+	    // First of all, we need to know if braces are correctly balanced.
+	    // This is not trivial due to variable interpolation which
+	    // occurs in heredoc, backticked and double quoted strings
+	    foreach (token_get_all('<?php ' . $code) as $token) {
+	        if (is_array($token)) {
+	            switch ($token[0]) {
+		            case T_CURLY_OPEN:
+		            case T_DOLLAR_OPEN_CURLY_BRACES:
+		            case T_START_HEREDOC: ++$inString; break;
+		            case T_END_HEREDOC:   --$inString; break;
+	            }
+	        }
+	        else if ($inString & 1) {
+	            switch ($token) {
+		            case '`':
+		            case '"': --$inString; break;
+	            }
+	        }
+	        else {
+	            switch ($token) {
+		            case '`':
+		            case '"': ++$inString; break;
+
+		            case '{': ++$braces; break;
+		            case '}':
+		                if ($inString) --$inString;
+		                else {
+		                    --$braces;
+		                    if ($braces < 0) break 2;
+		                }
+
+		                break;
+		            }
+	     	   }
+	    }
+
+	    // Display parse error messages and use output buffering to catch them
+	    $inString = @ini_set('log_errors', false);
+	    $token = @ini_set('display_errors', true);
+	    ob_start();
+
+	    // If $braces is not zero, then we are sure that $code is broken.
+	    // We run it anyway in order to catch the error message and line number.
+
+	    // Else, if $braces are correctly balanced, then we can safely put
+	    // $code in a dead code sandbox to prevent its execution.
+	    // Note that without this sandbox, a function or class declaration inside
+	    // $code could throw a "Cannot redeclare" fatal error.
+
+	    $braces || $code = "if(0){{$code}\n}";
+
+	    if (false === eval($code))
+	    {
+	        if ($braces) $braces = PHP_INT_MAX;
+	        else
+	        {
+	            // Get the maximum number of lines in $code to fix a border case
+	            false !== strpos($code, "\r") && $code = strtr(str_replace("\r\n", "\n", $code), "\r", "\n");
+	            $braces = substr_count($code, "\n");
+	        }
+
+	        $code = ob_get_clean();
+	        $code = strip_tags($code);
+
+	        // Get the error message and line number
+	        if (preg_match("'syntax error, (.+) in .+ on line (\d+)$'s", $code, $code))
+	        {
+	            $code[2] = (int) $code[2];
+	            $code = $code[2] <= $braces
+	                ? array($code[1], $code[2])
+	                : array('unexpected $end' . substr($code[1], 14), $braces);
+	        }
+	        else $code = array('syntax error', 0);
+	    }
+	    else
+	    {
+	        ob_end_clean();
+	        $code = false;
+	    }
+
+	    @ini_set('display_errors', $token);
+	    @ini_set('log_errors', $inString);
+
+	    return $code;
+	}
+	
+	
 ?>
--- scripts/phpepl.js
+++ scripts/phpepl.js
@@ -8,21 +8,63 @@
       liveURL = 'http://phpepl.cloudcontrolled.com/eval/index.php',
       
       // Set the html of the output div
+      // TODO: allow flag to set error class
       setOutput = function (text) {
-        $('.output span').html(text);
+        var isError = !! arguments[1],
+            $output = $('.output span');
+        
+        // Remove error classes if any
+        $output.html(text).removeClass('error');
+        
+        if (isError) {
+          $output.addClass('error');
+        }
+      },
+      
+      // Highlights the line in the gutter with the error
+      showLineError = function (line) {
+
+        // Find the dom element in the ace gutter
+        $('.ace_gutter-cell').each(function (idx) {
+          // If the cell's line number matches the error line
+          if (Number($(this).html()) === line) {
+            // Make the background red
+            $(this).addClass('error-gutter');
+            return;
+          }
+        });
       },
       
       processCode = function () {
         var code = editor.getValue();
-        window._gaq.push(['_trackEvent', 'Run']);
-                
+        
+        if (! code.length) {
+          setOutput("Please supply some code...");
+          return;  
+        }
+        
         $.ajax({
           type: "POST",
-          //url: devURL, 
+          // url: devURL,
           url: liveURL, 
           data: {code: code},
+          dataType: 'json',
           success: function (res) {
-            setOutput(res);
+            if (! res) return;
+            
+            var result  = res.result,
+                error   = res.error,
+                errorMsg;
+
+            if (error) {
+              // Show the line in red
+              showLineError(error.line);
+              // Show the error message
+              errorMsg = "Line " + error.line + ": " + error.message;
+              setOutput(errorMsg, true);
+              return;
+            }
+            setOutput(result);
           },
           error: function (jqXHR, textStatus, errorThrown) {
             setOutput("Whoopsie daisies!"); 
