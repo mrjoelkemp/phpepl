2eef4390cb6161572badef7485131d56a0b557f8
Joel Kemp <mrjoelkemp@gmail.com>
2012-12-18 00:58:23 -0500

    Added fatal error catching and prettying

--- scripts/phpepl.js
+++ scripts/phpepl.js
@@ -7,7 +7,7 @@
       devURL = 'http://localhost:8888/eval/index.php',
       liveURL = 'http://phpepl.cloudcontrolled.com/eval/index.php',
       editor,
-      
+
       // Format the time into a 12-hour, pretty format
       getTimeString = function () {
         var ct      = new Date(),
@@ -15,36 +15,36 @@
             minutes = ct.getMinutes(),
             seconds = ct.getSeconds(),
             timeString;
-          
+
         // Scale the hours back
         hours = (hours > 12) ? hours - 12 : hours;
 
         //if 00 then it is 12 am
         hours = hours === 0 ? 12 : hours;
-        
+
         timeString = [hours, minutes, seconds].join(':');
-            
+
         return timeString;
       },
-      
+
       // Set the html of the output div
       setOutput = function (text) {
         var isError = !! arguments[1],
             $output = $('.output span');
-            
+
         // Remove error classes if any
         $output.html(text).removeClass('error');
-        
+
         if (isError) {
           $output.addClass('error');
         }
-        
+
         // Turn off the spinner
         $('.spinner').fadeOut('fast');
         // Set the timestamp
         $('.timestamp').find('span').html(getTimeString());
       },
-      
+
       // Highlights the line in the gutter with the error
       showLineError = function (line) {
         // Find the dom element in the ace gutter
@@ -57,31 +57,59 @@
           }
         });
       },
-      
+
+      // Helper to show the fatal errors nicely
+      // Returns a list of the error text and line number that
+      // generated the error.
+      // FIXME: Implementation is fairly naive due to a lack of sample
+      //        fatal errors to guide the parsing logic
+      getPrettyFatalErrorMessage = function (responseText) {
+        var text = responseText,
+            tokensToReplace = ['\n', /<br \/>/g, /<b>/g,
+                                /<\/b>/g, /(Fatal error: +)/g],
+            splitTokens, err, line, lineNum;
+
+        $.each(tokensToReplace, function (idx, val) {
+          text = text.replace(val, "");
+        });
+
+        splitTokens = text.split('in');
+        err = splitTokens[0].trim();
+        splitTokens = text.split('on');
+        line = splitTokens[1].trim();
+
+        text = (err + ' on ' + line).trim();
+
+        lineNum = line.split(" ");
+        lineNum = Number(lineNum[1]);
+
+        return [text, lineNum];
+      },
+
       // Handles the sending of the code to the eval server
       processCode = function () {
         var code = editor.getValue();
-        
+
         if (! code.length) {
           setOutput("Please supply some code...");
-          return;  
+          return;
         }
-        
+
         $('.spinner').fadeIn('fast');
-        
+
         $.ajax({
           type: "POST",
           // url: devURL,
-          url: liveURL, 
+          url: liveURL,
           data: {code: code},
           dataType: 'json',
           success: function (res) {
             if (! res) return;
-            
+
             var result  = res.result,
                 error   = res.error,
                 errorMsg;
-            
+
             if (error) {
               // Show the line in red
               showLineError(error.line);
@@ -92,19 +120,21 @@
             }
             setOutput(result);
           },
-          error: function (jqXHR, textStatus, errorThrown) {            
-            setOutput("Whoopsie daisies!"); 
+          error: function (error) {
+            var text_line = getPrettyFatalErrorMessage(error.responseText);
+            setOutput(text_line[0], true);
+            showLineError(text_line[1]);
           }
-        }); 
+        });
       };
-      
+
   // Local storage helpers
-  var 
+  var
       saveCode = function () {
         if (window.localStorage) {
           var code = editor.getValue();
           localStorage.setItem('code', code);
-          
+
           // Show the saved message
           $('.timestamp')
             .find('span')
@@ -119,32 +149,32 @@
           editor.setValue(code);
         }
       };
-      
+
   $(function () {
     editor = ace.edit('editor');
     editor.setTheme('ace/theme/textmate');
     editor.getSession().setMode('ace/mode/php');
-    
+
     loadSavedCode();
-    
+
     $('.submit button').click(function () {
       processCode();
     });
-    
-    $(document).keydown(function (e) { 
-      // CMD + Enter or CTRL + Enter to run code   
-      if (e.which === 13 && (e.ctrlKey || e.metaKey)) {        
+
+    $(document).keydown(function (e) {
+      // CMD + Enter or CTRL + Enter to run code
+      if (e.which === 13 && (e.ctrlKey || e.metaKey)) {
         processCode();
         e.preventDefault();
       }
-      
+
       // CMD + S or CTRL + S to save code
       if (e.which === 83 && (e.ctrlKey || e.metaKey)) {
         saveCode();
         e.preventDefault();
       }
     });
-    
+
     // Remember the code in the editor
     // before navigating away
     $(window).unload(function () {
